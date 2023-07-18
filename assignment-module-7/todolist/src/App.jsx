cbddfb193330aa2f8f25516a8686fa6498beb288
Saidur Rahman <saidurwd@gmail.com>
2023-07-18 23:34:29 +0600

    assignment-module-7

---
+++ assignment-module-7/todolist/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TodoList } from './Component/TodoList.jsx';
+const App = () => {
+  return (
+    <div>
+      <h1>Todo List App</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
---
+++ assignment-module-7/todolist/src/Component/TodoList.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // Function to handle adding a new todo
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  // Function to handle removing a todo
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter a new todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodoList;
